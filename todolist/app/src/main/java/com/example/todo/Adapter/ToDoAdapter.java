103cb03a9214af194fc0b01dcda41ebf48e20ebe
mohd. siddique <siddiqmohd2772@gmail.com>
2024-07-09 22:45:55 +0530

    todolist

---
+++ todolist/app/src/main/java/com/example/todo/Adapter/ToDoAdapter.java
@@ -0,0 +1,113 @@
+package com.example.todo.Adapter;
+
+import android.content.Context;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.CheckBox;
+import android.widget.CompoundButton;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.todo.AddNewTask;
+import com.example.todo.MainActivity;
+import com.example.todo.Model.ToDoModel;
+
+
+import com.example.todo.R;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+import java.util.List;
+
+
+
+public class ToDoAdapter extends RecyclerView.Adapter<ToDoAdapter.MyViewHolder> {
+
+    private List<ToDoModel> todo;
+    private MainActivity activity;
+    private FirebaseFirestore firestore;
+
+    public ToDoAdapter(MainActivity mainActivity , List<ToDoModel> todo){
+        this.todo = todo;
+        activity = mainActivity;
+    }
+
+    @NonNull
+    @Override
+    public MyViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(activity).inflate(R.layout.each_task , parent , false);
+        firestore = FirebaseFirestore.getInstance();
+
+        return new MyViewHolder(view);
+    }
+
+    public void deleteTask(int position){
+        ToDoModel toDoModel = todo.get(position);
+        firestore.collection("task").document(toDoModel.TaskId).delete();
+        todo.remove(position);
+        notifyItemRemoved(position);
+    }
+    public Context getContext(){
+        return activity;
+    }
+    public void editTask(int position){
+        ToDoModel toDoModel = todo.get(position);
+
+        Bundle bundle = new Bundle();
+        bundle.putString("task" , toDoModel.getTask());
+        bundle.putString("due" , toDoModel.getDue());
+        bundle.putString("id" , toDoModel.TaskId);
+
+        AddNewTask addNewTask = new AddNewTask();
+        addNewTask.setArguments(bundle);
+        addNewTask.show(activity.getSupportFragmentManager() , addNewTask.getTag());
+    }
+    @Override
+    public void onBindViewHolder(@NonNull MyViewHolder holder, int position) {
+
+        ToDoModel toDoModel = todo.get(position);
+        holder.mCheckBox.setText(toDoModel.getTask());
+
+        holder.mDueDateTv.setText("Due On " + toDoModel.getDue());
+
+        holder.mCheckBox.setChecked(toBoolean(toDoModel.getStatus()));
+
+        holder.mCheckBox.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                if (isChecked){
+                    firestore.collection("task").document(toDoModel.TaskId).update("status" , 1);
+                }else{
+                    firestore.collection("task").document(toDoModel.TaskId).update("status" , 0);
+                }
+            }
+        });
+
+    }
+
+    private boolean toBoolean(int status){
+        return status != 0;
+    }
+
+    @Override
+    public int getItemCount() {
+        return todo.size();
+    }
+
+    public class MyViewHolder extends RecyclerView.ViewHolder{
+
+        TextView mDueDateTv;
+        CheckBox mCheckBox;
+
+        public MyViewHolder(@NonNull View itemView) {
+            super(itemView);
+
+            mDueDateTv = itemView.findViewById(R.id.due_date_tv);
+            mCheckBox = itemView.findViewById(R.id.mcheckbox);
+
+        }
+    }
+}
---
+++ todolist/app/src/main/java/com/example/todo/OnDialogCloseListner.java
@@ -0,0 +1,10 @@
+package com.example.todo;
+
+import android.content.DialogInterface;
+
+public interface OnDialogCloseListner {
+
+    void onDialogClose(DialogInterface dialogInterface);
+
+
+}
